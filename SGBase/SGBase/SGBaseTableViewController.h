369e184ce3fc626d7d3b378f9552073c349a9b8e
lyq <809996066@qq.com>
2018-09-03 17:06:56 +0800

    添加 SGBaseTableViewController

---
+++ SGBase/SGBase/SGBaseTableViewController.h
@@ -0,0 +1,63 @@
+//
+//  SGBaseTableViewController.h
+//  SGBase
+//
+//  Created by  on 2018/9/3.
+//  Copyright © 2018年 Rowling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGBaseTableViewController : UITableViewController
+
+/**
+ * 是否隐藏导航栏的那根黑线 YES:隐藏 NO:显示
+ * 注:在viewDidload里面设置好.
+ */
+@property (nonatomic,assign) BOOL hideNavLine;
+
+/**
+ * 一个用来做回掉的block，如果有需要在适当的地方去调用。
+ */
+@property(nonatomic,strong)  void(^commomBlock)(id x);
+
+/**
+ * 设置当前控制器导航栏左右的按钮。
+ */
+// 左
+@property (nonatomic,strong) UIButton * leftButton;
+// 右
+@property (nonatomic,strong) UIButton * rightButton;
+
+/**
+ * 设置当前控制器导航栏左右的按钮图片
+ * 注:当导航栏左/右按钮只有图片的时候可以使用该方法快速创建左右按钮。
+ */
+// 左
+-(void)setLeftButtonImage:(UIImage *)image;
+// 右
+-(void)setRightButtonImage:(UIImage *)image;
+
+/**
+ * 点击导航栏左按钮时执行的方法。需要实现了 -(void)setLeftButtonImage:(UIImage *)image 或者 给leftButton赋值才能有效
+ */
+-(void)leftButtonClick;
+
+/**
+ * 点击导航栏右按钮时执行的方法。需要实现了 -(void)setRightButtonImage:(UIImage *)image 或者 给rightButton赋值才能有效
+ */
+-(void)rightButtonClick;
+
+/**
+ * 返回按钮
+ * @param imgName 按钮图片名字
+ */
+-(void)addBackButton:(NSString *)imgName;
+
+/**
+ * 在设置了自定义导航返回按钮的时候点击返回按钮将要执行的方法。
+ * 注:如果需要在pop到上一级控制器的时候做某些操作可以在子类当中重写该方法。
+ */
+-(void)goback;
+
+@end
---
+++ SGBase/SGBase/SGBaseTableViewController.m
@@ -0,0 +1,133 @@
+//
+//  SGBaseTableViewController.m
+//  SGBase
+//
+//  Created by  on 2018/9/3.
+//  Copyright © 2018年 Rowling. All rights reserved.
+//
+
+#import "SGBaseTableViewController.h"
+#import "UIView+SGExtension.h"
+
+@interface SGBaseTableViewController ()
+
+@end
+
+@implementation SGBaseTableViewController
+
+#pragma -mark:隐藏导航栏的那根黑线
+//隐藏line
+- (void)NavgationBarLineHidden:(BOOL)hidden {
+    if ([self.navigationController.navigationBar respondsToSelector:@selector( setBackgroundImage:forBarMetrics:)]){
+        NSArray *list=self.navigationController.navigationBar.subviews;
+        for (id obj in list){
+            if ([UIDevice currentDevice].systemVersion.floatValue >= 10.0){
+                //10.0的系统字段不一样
+                UIView *view =   (UIView*)obj;
+                for (id obj2 in view.subviews) {
+                    if ([obj2 isKindOfClass:[UIImageView class]]){
+                        UIImageView *image =  (UIImageView*)obj2;
+                        if (image.sgHeight < 2) {
+                            image.hidden = hidden;
+                        }
+                    }
+                }
+            }else{
+                if ([obj isKindOfClass:[UIImageView class]]) {
+                    UIImageView *imageView = (UIImageView *)obj;
+                    NSArray *list2 = imageView.subviews;
+                    for (id obj2 in list2) {
+                        if ([obj2 isKindOfClass:[UIImageView class]]) {
+                            UIImageView *imageView2 = (UIImageView *)obj2;
+                            if (imageView2.sgHeight < 2) {
+                                imageView2.hidden = hidden;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#pragma -mark:生命周期
+-(void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    // 判断是否需要隐藏黑线
+    if (self.hideNavLine) {
+        [self NavgationBarLineHidden:YES];
+    }
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    // 在该控制器消失前，恢复黑线显示
+    if (self.hideNavLine) {
+        [self NavgationBarLineHidden:NO];
+    }
+}
+
+#pragma -mark:导航栏左右按钮设置
+// 导航左按钮
+-(void)setLeftButton:(UIButton *)leftButton {
+    _leftButton = leftButton;
+    [_leftButton addTarget:self action:@selector(leftButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:leftButton];
+}
+
+-(void)setLeftButtonImage:(UIImage *)image {
+    UIButton * btn = [[UIButton alloc]init];
+    [btn setImage:image forState:UIControlStateNormal];
+    [btn sizeToFit];
+    [self setLeftButton:btn];
+}
+// 导航右按钮
+-(void)setRightButton:(UIButton *)rightButton {
+    _rightButton = rightButton;
+    [_rightButton addTarget:self action:@selector(rightButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:rightButton];
+}
+
+-(void)setRightButtonImage:(UIImage *)image {
+    UIButton * btn = [[UIButton alloc]init];
+    [btn setImage:image forState:UIControlStateNormal];
+    [btn sizeToFit];
+    [self setRightButton:btn];
+}
+
+-(void)leftButtonClick {
+}
+
+-(void)rightButtonClick {
+}
+
+#pragma -mark:设置有导航栏的情况下普遍返回按钮的样式
+-(void)addBackButton:(NSString *)imgName{
+    if (self.navigationController.childViewControllers.count > 1) { // 非根控制器
+        self.navigationItem.leftBarButtonItem = [self itemWithimage:[UIImage imageNamed:imgName] selImage:[UIImage imageNamed:imgName] target:self action:@selector(goback)];
+    }
+}
+
+- (UIBarButtonItem *)itemWithimage:(UIImage *)image selImage:(UIImage *)selImage target:(id)target action:(SEL)action{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setImage:image forState:UIControlStateNormal];
+    btn.sgSize = CGSizeMake(40, 40);
+    btn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    [btn addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    return [[UIBarButtonItem alloc] initWithCustomView:btn];
+}
+
+-(void)goback {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+-(void)dealloc {
+    NSLog(@"[%@ is dealloced]",NSStringFromClass([self class]));
+}
+
+@end
